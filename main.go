93a6bab6a4a540eb339d55ac84fd24a32c035078
Lee Hull <lhull@hcpci.com>
2020-02-19 18:26:37 -0500

    Changes unrelated to a JIRA task

--- main.go
+++ main.go
@@ -23,7 +23,7 @@ func handleRequests() {
 	}
 
 	http.HandleFunc("/", homePage)
-	fmt.Println("Listening...")
+	fmt.Println("Listening....")
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
 }
 
